1ed02b049eb209a20ae2ef4a6cde8aa2dc7b97e8
Piotr Gębalski <piotrgebalski5@gmail.com>
2023-05-01 12:23:13 +0200

    Add some tests for offer controller

---
+++ TG.Backend.Tests/OfferControllerTests/OfferControllerTests.cs
@@ -0,0 +1,117 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TG.Backend.Data;
+using FluentAssertions;
+using Newtonsoft.Json;
+using TG.Backend.Models.Offer;
+
+namespace TG.Backend.Tests.OfferControllerTests;
+
+public class OfferControllerTests : IClassFixture<WebApplicationFactory<Program>>, IDisposable
+{
+    private readonly HttpClient _client;
+    private WebApplicationFactory<Program> _factory;
+
+    public OfferControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var dbContextOptions = services.SingleOrDefault(service =>
+                        service.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                    if (dbContextOptions is not null)
+                        services.Remove(dbContextOptions);
+                    services.AddDbContext<AppDbContext>(op =>
+                        op.UseNpgsql(
+                            "Server=localhost;Port=5432;Database=tg-backend-test;User Id=postgres;Password=password"));
+                });
+            });
+        _client = _factory.CreateClient();
+    }
+
+    [Theory]
+    [InlineData("drive=amg&gearbox=pwd&priceHigh=20&priceLow=10")]
+    [InlineData("drive=fwd&gearbox=fdf&priceHigh=20&priceLow=10")]
+    [InlineData("drive=kgm&gearbox=manual&priceHigh=20&priceLow=10")]
+    public async Task GetOffers_ForInvalidFilters_ReturnsBadRequestResult(string queryParams)
+    {
+        // act
+        var response = await _client.GetAsync($"/Offer?{queryParams}");
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Theory]
+    [InlineData("drive=fwd&gearbox=automatic&priceHigh=20&priceLow=10")]
+    [InlineData("drive=rwd&gearbox=manual&priceHigh=20&priceLow=10")]
+    [InlineData("drive=fwd&gearbox=manual&priceHigh=20&priceLow=10")]
+    public async Task GetOffers_ForValidFilters_ReturnsOkResult(string queryParams)
+    {
+        // act
+        var response = await _client.GetAsync($"/Offer?{queryParams}");
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetOfferBydId_ForValidId_ReturnsOkResult()
+    {
+        // arrange
+        var offer = new Offer
+        {
+            Price = 50,
+            Description = "desc",
+            ContactEmail = "test@test.com",
+            ContactPhoneNumber = "123456789",
+            Vehicle = new Vehicle
+            {
+                Name = "name",
+                Description = "desc",
+                ProductionStartYear = 1950,
+                ProductionEndYear = 2000,
+                NumberOfDoors = 1,
+                NumberOfSeats = 1,
+                BootCapacity = 1,
+                Length = 1,
+                Height = 1,
+                Width = 1,
+                WheelBase = 1,
+                BackWheelTrack = 1,
+                FrontWheelTrack = 1,
+                Gearbox = Gearbox.Automatic,
+                Drive = Drive.AWD,
+            }
+        };
+        await Seed(offer);
+        
+        // act
+        var response = await _client.GetAsync($"Offer/{offer.Id}");
+        
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Content.Headers.ContentType!.MediaType.Should().Be("application/json");
+    }
+    
+    public void Dispose()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        context.Database.EnsureDeleted();
+        _client.Dispose();
+        _factory.Dispose();
+    }
+
+    private async Task Seed(Offer offer)
+    {
+        using var scope = _factory.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await context.Offers.AddAsync(offer);
+        await context.SaveChangesAsync();
+    }
+}
