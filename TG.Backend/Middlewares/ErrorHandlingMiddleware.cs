4475c3c93ba843d29a67fa47609660f0a6a6b829
ptakpiotr <piter020@outlook.com>
2023-06-13 18:53:12 +0200

    Temporary middleware removal

--- TG.Backend/Middlewares/ErrorHandlingMiddleware.cs
+++ TG.Backend/Middlewares/ErrorHandlingMiddleware.cs
@@ -1,30 +1,29 @@
-using FluentValidation;
-using TG.Backend.Exceptions;
-
 namespace TG.Backend.Middlewares;
 
 public class ErrorHandlingMiddleware : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        try
-        {
-            await next.Invoke(context);
-        }
-        catch (NotFoundException notFoundException)
-        {
-            context.Response.StatusCode = StatusCodes.Status404NotFound;
-            await context.Response.WriteAsync($"Content with Id = ${notFoundException.Id} was not found");
-        }
-        catch (ValidationException validationException)
-        {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            await context.Response.WriteAsJsonAsync(validationException.Errors);
-        }
-        catch (Exception exception)
-        {
-            context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
-            await context.Response.WriteAsync(exception.Message);
-        }
+        //try
+        //{
+        //    await next.Invoke(context);
+        //}
+        //catch (NotFoundException notFoundException)
+        //{
+        //    context.Response.StatusCode = StatusCodes.Status404NotFound;
+        //    await context.Response.WriteAsync($"Content with Id = ${notFoundException.Id} was not found");
+        //}
+        //catch (ValidationException validationException)
+        //{
+        //    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        //    await context.Response.WriteAsJsonAsync(validationException.Errors);
+        //}
+        //catch (Exception exception)
+        //{
+        //    context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+        //    await context.Response.WriteAsync(exception.Message);
+        //}
+
+        return Task.CompletedTask;
     }
 }
